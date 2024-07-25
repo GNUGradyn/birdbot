7e5ab9b7215fbe43899800c73ad67f015c15f381
Gradyn Wursten <me@gradyn.com>
2024-07-24 21:57:40 -0600

    potential fix

--- BirdBot/AITools.cs
+++ BirdBot/AITools.cs
@@ -6,7 +6,7 @@ public class AITools
 {
     public static string ship24bearer; // TODO: figure out a way to expose this with dependency injection instead of this
     
-    [FunctionDescription(Name = "porch", Description = "Attaches a live snapshot of the porch from a camera to the next message")]
+    [FunctionDescription(Name = "porch", Description = "Attaches a live snapshot of the porch from a camera to the current message")]
     public async Task<byte[]> PorchCamera()
     {
         using (var httpClient = new HttpClient())
